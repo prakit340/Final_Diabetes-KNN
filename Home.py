a35ad25ba2d45edcdf73d3ea81d564e081c88942
Prakit Junkhum <prakit340@hotmail.com>
2023-10-30 00:00:20 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,6 +6,16 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
 
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background-color: gray;
+    }
+   </style>
+    """,
+    unsafe_allow_html=True
+)
 st.image('./images/header.jpg')
 
 st.title('การจำแนกโรคเบาหวาน')
