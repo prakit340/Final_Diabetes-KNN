e548dfd576b2cd83539cb2f9365693663befb824
Prakit Junkhum <prakit340@hotmail.com>
2023-10-30 00:56:43 +0700

    Update 2_🗂 Theory.py

--- pages/2_🗂 Theory.py
+++ pages/2_🗂 Theory.py
@@ -29,11 +29,14 @@ st.write('''โรคเบาหวานเป็นปัญหาสุข�
 
 st.image('./images/diabetes.jpg')
 
-# Insert containers separated into tabs:
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("this is tab 1")
-tab2.write("this is tab 2")
+col1, col2 = st.columns(2)
+col1.write('Column 1')
+col2.write('Column 2')
 
-# You can also use "with" notation:
-with tab1:
-    st.radio('Select one:', [1, 2])
+# Three columns with different widths
+col1, col2, col3 = st.columns([3,1,1])
+# col1 is wider
+
+# Using 'with' notation:
+with col1:
+    st.write('This is column 1')
