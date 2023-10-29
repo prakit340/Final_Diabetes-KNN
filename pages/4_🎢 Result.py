7b4f5a943d430efd667c58be507f4b90a2f8814a
Prakit Junkhum <prakit340@hotmail.com>
2023-10-30 02:03:28 +0700

    Update 4_🎢 Result.py

--- pages/4_🎢 Result.py
+++ pages/4_🎢 Result.py
@@ -29,10 +29,68 @@ df=pd.read_csv('./data/diabetes.csv')
 st.header('ตัวอย่างข้อมูล')
 st.write(df.head(10))
 
+st.markdown("""<hr style="height:5px;border:none;color:#FF4B4B;background-color:#FF4B4B;" /> """, unsafe_allow_html=True)
+
 st.header('Plot จากค่าของ Outcome ระหว่าง Diabetes กับ Normal ที่แต่ละ Attribute')
 fig = sns.pairplot(df,hue='Outcome')
 st.pyplot(fig)
 
+st.markdown("""<hr style="height:5px;border:none;color:#FF4B4B;background-color:#FF4B4B;" /> """, unsafe_allow_html=True)
+
 st.header("ศึกษาตัวแปรใดมีผลกับโรคเบาหวาน โดยใช้ตาราง Heatmap")
 corr = df.corr()
 st.write(corr.style.background_gradient(cmap='coolwarm'))
+
+st.markdown("""<hr style="height:5px;border:none;color:#FF4B4B;background-color:#FF4B4B;" /> """, unsafe_allow_html=True)
+
+x = df.iloc[:, 0:8]     # Select all row and column with 0-7
+y = df['Outcome']       # Column Outcome for Output
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)  # Split data 80:20
+
+scaler = StandardScaler()
+x_train=scaler.fit_transform(x_train)
+x_test=scaler.transform(x_test)
+
+dtModel = DecisionTreeClassifier(criterion='gini')    # Use Decision tree algorithm
+dtModel.fit(x_train, y_train)                         # Train
+
+nbModel = GaussianNB()                               # Use Naive Bayes algorithm
+nbModel.fit(x_train, y_train)
+
+knnModel = KNeighborsClassifier(n_neighbors=15, metric='euclidean', p=2)        # Use KNN algorithm
+knnModel.fit(x_train, y_train)
+
+################################ Predict
+
+y_predDT = dtModel.predict(x_test)                  # Predict with x_test
+accuracyDT = accuracy_score(y_test, y_predDT)
+
+y_predNB = nbModel.predict(x_test)
+accuracyNB = accuracy_score(y_test, y_predNB)
+
+y_predKNN = knnModel.predict(x_test)
+accuracyKNN = accuracy_score(y_test, y_predKNN)
+
+y_target = ['Diabetes', 'Normal']
+clfDT_report = classification_report(y_test, y_predDT, target_names = y_target)
+
+clfNB_report = classification_report(y_test, y_predNB, target_names = y_target)
+
+clfKNN_report = classification_report(y_test, y_predKNN, target_names = y_target)
+
+st.header("ผลลัพธ์ Confusuin Matrix")
+matrix = confusion_matrix(y_test, y_predKNN)
+plt.figure(figsize=(10, 8))
+fig2 = sns.heatmap(matrix, annot=True)
+st.pyplot(fig2)
+
+st.markdown("""<hr style="height:5px;border:none;color:#FF4B4B;background-color:#FF4B4B;" /> """, unsafe_allow_html=True)
+
+st.write("ความแม่นยำของ Decision Tree Classifier:", '{:.2f}'.format(accuracyDT))
+st.write(clfDT_report)
+
+st.write("ความแม่นยำของ Naive Bayes Tree Classifier:", '{:.2f}'.format(accuracyNB))
+st.write(clfNB_report)
+
+st.write("ความแม่นยำของ K-Nearest Neighbors (KNN) Classifier:", '{:.2f}'.format(accuracyKNN))
+st.write(clfKNN_report)
