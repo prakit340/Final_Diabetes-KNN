a4f7dd753b1542e4bb0bbd118395943c20720b0a
Prakit Junkhum <prakit340@hotmail.com>
2023-10-30 01:49:16 +0700

    Update 4_🎢 Result.py

--- pages/4_🎢 Result.py
+++ pages/4_🎢 Result.py
@@ -1,8 +1,17 @@
-import json
-import time
-import requests
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.tree import DecisionTreeClassifier          # Import Decision Tree Classifier
+from sklearn.naive_bayes import GaussianNB              # Import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier       # Import KNN
+from sklearn.model_selection import train_test_split    # Import train_test_split function
+from sklearn.preprocessing import StandardScaler
+
+from sklearn import metrics
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score    # Import measure performance function
+from sklearn.metrics import classification_report, confusion_matrix
 
 st.set_page_config(
     page_title="Prakit Junkhum 633230010 24.2",
@@ -12,9 +21,16 @@ st.set_page_config(
 st.image('./images/header.jpg')
 st.sidebar.markdown("# Result 🎢")
 
-st.header("Show Data Index Price")
-df=pd.read_csv("./data/stock_index_price.csv")
+st.title("ผลการดำเนินงาน")
+st.write("")
+
+df=pd.read_csv('./data/diabetes.csv')
+
+st.text('ตัวอย่างข้อมูล')
 st.write(df.head(10))
+
+st.text('Plot จากค่าของ Outcome ระหว่าง Diabetes กับ Normal ที่แต่ละ Attribute')
+st.write(sns.pairplot(df,hue='Outcome'))
 
 st.header("Show Chart")
 st.line_chart(
